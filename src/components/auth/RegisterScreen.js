ac1fddbca7e91be33778c7e03485babd0ce2336e
David Toledo <dtoledoz0207@gmail.com>
2021-03-18 11:11:03 -0600

    adds-useForm-hook-in-RegisterScreen-component-to-get-form-data

--- src/components/auth/RegisterScreen.js
+++ src/components/auth/RegisterScreen.js
@@ -1,17 +1,34 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useForm } from '../../hooks/useForm';
 
 
 const RegisterScreen = () => {
+
+  const [formValues, handleInputChange] = useForm({
+    name: 'Alex',
+    email: 'alex0@gmail.com',
+    password: '123456',
+    password2: '123456'
+  });
+
+  const {name, email, password, password2} = formValues;
+
+
+  const handleRegister = (event) => {
+    event.preventDefault();
+    console.log(name, email, password2);
+  }
+
   return(
     <>
       <h3 className="auth__title">login</h3>
       
-      <form>
-        <input className="auth__input" type="text" placeholder="Name" name="name" autoComplete="off" />
-        <input className="auth__input" type="email" placeholder="Email" name="email" autoComplete="off" />
-        <input className="auth__input" type="password" placeholder="Password" name="password" />
-        <input className="auth__input" type="password" placeholder="Confirm password" name="password2" />
+      <form onSubmit={handleRegister}>
+        <input className="auth__input" type="text" placeholder="Name" name="name" value={name} onChange={handleInputChange} autoComplete="off" />
+        <input className="auth__input" type="email" placeholder="Email" name="email" value={email} onChange={handleInputChange} autoComplete="off" />
+        <input className="auth__input" type="password" placeholder="Password" name="password" value={password} onChange={handleInputChange} />
+        <input className="auth__input" type="password" placeholder="Confirm password" name="password2" value={password2} onChange={handleInputChange} />
 
         <button type="submit" className="btn btn-primary btn-block mb-5">Register</button>
 
