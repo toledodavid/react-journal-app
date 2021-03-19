4b64c023fe042d5fa5627a578d450b5932e14287
David Toledo <dtoledoz0207@gmail.com>
2021-03-19 12:01:47 -0600

    keep-authentication-when-reload-page-AppRouter

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,17 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {
   BrowserRouter as Router,
   Switch,
   Route,
   Redirect
 } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+
+import { firebase } from '../firebase/firebase-config';
 
 import AuthRouter from './AuthRouter';
 import JournalScreen from '../components/journal/JournalScreen';
+import { loginAction } from '../actions/authActions';
+
 
 
 
 const AppRouter = () => {
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user?.uid) {
+        dispatch(loginAction(user.uid, user.displayName));
+      }
+    });
+  }, [dispatch]);
+
   return(
     <Router>
       <div>
