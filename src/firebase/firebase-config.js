eff4c0555019819abae5ff1ddd953e27ea71a0c9
David Toledo <dtoledoz0207@gmail.com>
2021-04-06 16:29:25 -0500

    Adding-custom-enviroment-variables

--- src/firebase/firebase-config.js
+++ src/firebase/firebase-config.js
@@ -4,37 +4,47 @@ import 'firebase/auth';
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBn2qrVlKCumoEPA-DLdzCgu759l0SEeCc",
-  authDomain: "react-app-courses-6e136.firebaseapp.com",
-  projectId: "react-app-courses-6e136",
-  storageBucket: "react-app-courses-6e136.appspot.com",
-  messagingSenderId: "332851451441",
-  appId: "1:332851451441:web:297db2c628ba9e2305a710"
+  apiKey: process.env.REACT_APP_APIKEY,
+  authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASEURL,
+  projectId: process.env.REACT_APP_PROJECTID,
+  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_APPID
 };
 
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBn2qrVlKCumoEPA-DLdzCgu759l0SEeCc",
+//   authDomain: "react-app-courses-6e136.firebaseapp.com",
+//   projectId: "react-app-courses-6e136",
+//   storageBucket: "react-app-courses-6e136.appspot.com",
+//   messagingSenderId: "332851451441",
+//   appId: "1:332851451441:web:297db2c628ba9e2305a710"
+// };
 
-const firebaseConfigTesting = {
-  apiKey: "AIzaSyAxP5JigMdg3ydoBUsxODMZh3-Q7ylJEMw",
-  authDomain: "heroesappmac.firebaseapp.com",
-  databaseURL: "https://heroesappmac.firebaseio.com",
-  projectId: "heroesappmac",
-  storageBucket: "heroesappmac.appspot.com",
-  messagingSenderId: "744982426600",
-  appId: "1:744982426600:web:5fcf7a0385216f2185aa5d"
-};
 
-if (process.env.NODE_ENV === 'test') {
-  // testing
-  // Initialize Firebase
-firebase.initializeApp(firebaseConfigTesting);
-} else {
-  // dev / prod
-  // Initialize Firebase
+// const firebaseConfigTesting = {
+//   apiKey: "AIzaSyAxP5JigMdg3ydoBUsxODMZh3-Q7ylJEMw",
+//   authDomain: "heroesappmac.firebaseapp.com",
+//   databaseURL: "https://heroesappmac.firebaseio.com",
+//   projectId: "heroesappmac",
+//   storageBucket: "heroesappmac.appspot.com",
+//   messagingSenderId: "744982426600",
+//   appId: "1:744982426600:web:5fcf7a0385216f2185aa5d"
+// };
+
+// if (process.env.NODE_ENV === 'test') {
+//   // testing
+//   // Initialize Firebase
+// firebase.initializeApp(firebaseConfigTesting);
+// } else {
+//   // dev / prod
+//   // Initialize Firebase
+// firebase.initializeApp(firebaseConfig);
+// }
+
+
 firebase.initializeApp(firebaseConfig);
-}
-
-
-
 
 const db = firebase.firestore();
 const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
