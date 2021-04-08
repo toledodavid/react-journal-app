9e20486222b9db881bd359c9fc6baeab6776777a
David Toledo <dtoledoz0207@gmail.com>
2021-04-08 13:14:23 -0500

    create-tests-for-startGoogleLoginAction-and-startLoginEmailPassword-in-LoginScreen-component

--- src/tests/components/auth/LoginScreen.test.js
+++ src/tests/components/auth/LoginScreen.test.js
@@ -5,7 +5,13 @@ import LoginScreen from '../../../components/auth/LoginScreen';
 import configureStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
 import { MemoryRouter } from 'react-router-dom';
+import { startGoogleLoginAction, startLoginEmailPassword } from '../../../actions/authActions';
 
+
+jest.mock('../../../actions/authActions', () => ({
+  startGoogleLoginAction: jest.fn(),
+  startLoginEmailPassword: jest.fn()
+}));
 
 const middlewares = [thunk];
 const mockStore = configureStore(middlewares);
@@ -19,6 +25,7 @@ const initState = {
 };
 
 let store = mockStore(initState);
+store.dispatch = jest.fn();
 
 const wrapper = mount(
   <Provider store={store}>
@@ -33,10 +40,22 @@ describe('Tests in <LoginScreen /> component', () => {
 
   beforeEach(() => {
     store = mockStore(initState);
+    jest.clearAllMocks();
   });
 
   test('It should render correctly', () => {
     expect(wrapper).toMatchSnapshot();
   });
 
+  test('It should dispatch startGoogleLoginAction', () => {
+    wrapper.find('.google-btn').prop('onClick')();
+
+    expect(startGoogleLoginAction).toHaveBeenCalled();
+  });
+
+  test('It should dispatch startLoginEmailPassword with email and password', () => {
+    wrapper.find('form').prop('onSubmit')({preventDefault(){}});
+
+    expect(startLoginEmailPassword).toHaveBeenCalledWith('alex0@gmail.com', '123456');
+  });
 });
